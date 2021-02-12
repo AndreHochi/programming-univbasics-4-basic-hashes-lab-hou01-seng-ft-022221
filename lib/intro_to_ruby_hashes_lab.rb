c7ae7c7855c02c053ade0ae86cebfea1881ebbef
Andre Hochi <andrehochiwork@gmail.com>
2021-02-12 01:54:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,15 +3,15 @@ def new_hash
 end
 
 def my_hash
-  hash = {:key: 'value'}
+  hash = {key: 'value'}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  hash = {:name: 'Grace Hopper'}
+  hash = {name: 'Grace Hopper'}
 end
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  hash = {:id: 'number'}
+  hash = {id: 'number'}
 end
